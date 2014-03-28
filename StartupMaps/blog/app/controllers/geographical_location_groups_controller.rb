f3d63e100a95e095d404093f4ee0456379d01f94
maha <maha@ubuntu.(none)>
2014-03-28 11:48:58 +0000

    add_my_user_story

--- StartupMaps/blog/app/controllers/geographical_location_groups_controller.rb
+++ StartupMaps/blog/app/controllers/geographical_location_groups_controller.rb
@@ -1,2 +1,16 @@
 class GeographicalLocationGroupsController < ApplicationController
+
+	def new
+	end
+
+ # this method creates a new group and saves it given the location as input from the user
+ # input: string location
+ # author: Maha Salah
+def create
+  @geo = GeographicalLocationGroup.new(params[:GeographicalLocationGroups])
+  
+  @geo.save 
+  redirect_to @geo 
+end
+
 end
