c0eaac5522364967df3fff3a7955028ec8dc2f3b
amrgamal18 <amrjimmy18@hotmail.com>
2014-03-28 19:06:20 +0200

    added create project

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,2 +1,23 @@
 class ProjectsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		]
+		 @project = Projects.new(project_params)
+		 @project.save
+		 redirect_to @project
+	end
+
+	
+
+	def show
+		@project = Projects.find(params[:id])
+	end
+	private
+	def project_params
+		params.require(:project).permit(:name, :category, :location, :discription)
+	end
 end
