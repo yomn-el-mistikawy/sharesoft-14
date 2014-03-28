cd2271a0983fe19ab101bcac88c32b1dfee43d36
Hana Magdy <hana.hosny94@gmail.com>
2014-03-28 18:26:00 +0200

    added my user story

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+	  
+#update the project by id, then enters table "projects" along with goals,milestone & requirements
+#and gets edited, Also redirected  to edit (which is a blank page)
+def update
+	@project = Project.find(params[:id])
+  if @project.update(params[:projects].permit(:goals, :milestones, :requiements))
+    
+    redirect_to @project
+  else
+    render 'edit'
+  end
+end
+
+#edits the project, to the 
+#the link "localhost:3000/projects/1/edit"
+def edit
+  @project = Project.find(1)
+end
+
+
+#The view can not acess the methods under private 
+private
+  def project_params
+    params.require(:projects).permit(:goals, :milestones, :requiements)
+  #This makes the goals,milestones and requirements accessable by/ visable to the method update"
+  end
 end
--- StartupMaps/blog/db/migrate/20140328091305_create_internships_entities.rb
+++ StartupMaps/blog/db/migrate/20140328091305_create_internships_entities.rb
@@ -2,8 +2,8 @@ class CreateInternshipsEntities < ActiveRecord::Migration
   def change
     create_table :internships_entities do |t|
       t.integer :entity_id
-      t.String :name
-      t.String :description
+      t.string :name
+      t.string :description
       t.boolean :available
 
       t.timestamps
