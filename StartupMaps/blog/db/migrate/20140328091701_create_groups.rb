024eeaaffd1623a16a71f62e450507e4db2131d3
samarfikri <samar.alezaby@gmail.com>
2014-03-28 23:01:57 +0200

    Samar's commit.. GroupAddDescription

--- StartupMaps/blog/db/migrate/20140328091701_create_groups.rb
+++ StartupMaps/blog/db/migrate/20140328091701_create_groups.rb
@@ -1,12 +1,7 @@
-class CreateGroups < ActiveRecord::Migration
-  def change
-    create_table :groups do |t|
-      t.String :name
-      t.String :location
-      t.datetime :dateCreated
-      t.String :description
-      t.integer :numberOfMembers
-      t.integer :groupCreatorId
+class CreateGroups < ActiveRecord::Migration   def change     create_table
+:groups do |t|       t.String :name       t.String :location       t.datetime
+:dateCreated       t.String :description       t.integer :numberOfMembers
+t.integer :groupCreatorId
 
       t.timestamps
     end
