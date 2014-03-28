b401dec85d30ca225ab708b69e9145132f5279c3
Alia Tarek <alia@ubuntu.(none)>
2014-03-28 20:26:52 +0200

    Alia - showLayers

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -1,2 +1,78 @@
 class MapsController < ApplicationController
+
+#gets all startups in the db
+#Input : Startup Table
+#Output : online Startups
+#Author : Alia
+
+def showStartups
+		@startups= Startup.all
+	end
+
+#gets all Investors in the db
+#Input : Investors Table
+#Output : all Investors
+#Author : Alia
+
+def showInvestors
+	@investors = Investor.all
 end
+#gets all Services in the db
+#Input : Services Table
+#Output : all Services
+#Author : Alia
+
+def showServices
+	@services = Service.all
+end
+#gets all merged startups in the db
+#Input : Startup Table
+#Output : merged Startups
+#Author : Alia
+
+def showMerged
+
+		@merged = Startup.where(:joint_ventures != "")
+end
+#gets all launched startups in the db
+#Input : Startup Table
+#Output : launched Startups
+#Author : Alia
+
+def showLaunched
+		@launched = Startup.where(:launched_Status => true)
+end
+#gets all not yet launched in the db
+#Input : Startup Table
+#Output : not yet launched Startups
+#Author : Alia
+
+
+def showNotLaunched
+
+@notlaunched= Startup.where(:launched_Status => false)
+end
+
+
+#gets all online startups in the db
+#Input : Startup Table
+#Output : online Startups
+#Author : Alia
+
+def showOnline
+
+@online = Startup.where(:online_Status => true)
+end
+#gets all offline startups in the db
+#Input : Startup Table
+#Output : onffline Startups
+#Author : Alia
+
+def showOffline
+
+	@offline = Startup.where(:online_Status => false)
+end
+
+end
+
+
--- StartupMaps/blog/config/routes.rb
+++ StartupMaps/blog/config/routes.rb
@@ -1,4 +1,20 @@
 Blog::Application.routes.draw do
+  get "welcome/index"
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+ resources :maps do
+get "showStartups" 
+get "showInvestors"
+get "showServices"
+get "showLaunched"
+get "showNotLaunched"
+get "showOffline"
+get "showOnline"
+
+ end
+ resources :startups
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
