19afaae44a6660a148ce5aea093b5fe1f8293b8c
Ahmed Yousif <ahmedyousif@ubuntu.(none)>
2014-03-29 00:57:48 +0300

    i commited

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,2 +1,16 @@
 class ProjectsController < ApplicationController
+		end
+		    =begin
+			this method sends merge request to others startups
+			= end
+	def sendrequest
+		#this is the request reciever project
+		@projectR= Project.find(1)
+		#this is the request sender reciever
+		@projectS= Project.find(2)
+		
+     	@startupsS_id= @ProjectS.startups_id
+    	@startupsR_id= @ProjectR.startups_id
+     	@sendRequest= Sendrequest.new(:startupsS_id=>@startups_id, :startupsR_id=>@startups_id, :projectR=>@ProjectR.id, 
+	end
 end
