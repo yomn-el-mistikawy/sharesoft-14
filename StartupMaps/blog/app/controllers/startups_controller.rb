95db3f3de61522521c0f2d09f043c2496fae3384
sandra <sandrausf92@gmail.com>
2014-03-28 19:14:03 +0200

    added check years

--- StartupMaps/blog/app/controllers/startups_controller.rb
+++ StartupMaps/blog/app/controllers/startups_controller.rb
@@ -1,2 +1,17 @@
 class StartupsController < ApplicationController
+	def index
+  @comp = Startups.all
+end
+
+
+def list
+	#render text: params[:startup].inspect
+	@res = Startups.where(:NumberOfYears =>params[:snumber_of_working_years])
+end
+
+
+
+
+def show
+end
 end
