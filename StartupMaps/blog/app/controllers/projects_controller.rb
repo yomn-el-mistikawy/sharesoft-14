06d4fbc11fdb11347790d0e02b355f191fe367a0
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-04-13 00:41:05 +0200

    modified some mistakes in the code

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -6,18 +6,22 @@ class ProjectsController < ApplicationController
 	Output: @suggested
 	Author: Yomn El-Mistikawy
 =end
+
 def suggest
 	@project= Project.find(1)
-    @suggested= Project.where(:location => "cairo", :category => "baking").where.not(:id => "1", :startup_id => "1")
+  @suggested= Project.where(:location => "cairo", :category => "baking").where.not(:id => "1", :startup_id => "1")
 end
+
+
 =begin
 	This method shows the profile of the selected suggested project
 	Input: selected project id
 	Output: view showing the profile of the project with a merge request button
 	Author: Yomn El-Mistikawy
 =end
-def showSuggested
-    @project= Project.find(params[:project])
+
+def show_suggested
+  @project= Project.find(params[:project])
 end
 
 
@@ -26,7 +30,6 @@ def new
 end
 
 def create
-		]
 	@project = Projects.new(project_params)
 	@project.save
 	redirect_to @project
