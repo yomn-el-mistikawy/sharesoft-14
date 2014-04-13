ebf54ffc51a781dc38b5269f8b440fe48268ce80
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-04-14 00:13:32 +0200

    added_merge_request_route

--- StartupMaps/blog/config/routes.rb
+++ StartupMaps/blog/config/routes.rb
@@ -6,7 +6,7 @@ Blog::Application.routes.draw do
   resources :projects do
      get "suggest"
      get "show_suggested"
-     post "temp"
+     get "merge_request"
   end
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
