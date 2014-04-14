6019461c82761650ce957ca2294018a37908ebe5
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-04-14 12:36:41 +0200

    I readded my code

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,30 +1,26 @@
 class ProjectsController < ApplicationController
 
-=begin
-	This methods queries the table project for projects that are in the same category and location as the current project
-	Input: project_ID
-	Output: @suggested
-	Author: Yomn El-Mistikawy
-=end
+	# Defintion: This method takes the following input 
+	# and calls get_suggested() to show a list of suggested 
+	# projects to the user.
+	# Input: project_id
+	# Output: project_id, suggested_project
+	# Author: Yomn El-Mistikawy
 
-def suggest
-	@suggested = Project.get_suggest( Project.find( params[:project_id] ) )
-end
+	def suggest
+		@suggested = Project.get_suggest(Project.find(params[:project_id]))
+	end
 
 
-=begin
-	This method shows the profile of the selected suggested project
-	Input: selected project id
-	Output: view showing the profile of the project with a merge request button
-	Author: Yomn El-Mistikawy
-=end
+	## Defintion: This method takes the following input 
+	# and calls get_suggested() to show a list of suggested 
+	# projects to the user.
+	# Input: project_id
+	# Output: project_id, suggested_project
+	# Author: Yomn El-Mistikawy
+	## End 
 
-def show_suggested
-  @project = Project.find( params[:suggested_project] )
-end
-
-def temp
-	@suggested_project = Project.find( params[:suggested_project] )
-	@project = Project.find( params[:project_id] )
-end
+	def show_suggested
+  	@project = Project.find(params[:suggested_project])
+	end
 end
