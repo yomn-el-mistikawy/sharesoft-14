5b4f9ea062bd4b85f9b4e8010aabfac2d345d44b
sarah-fathallah <sfathallah.sf@gmail.com>
2014-04-15 17:23:21 +0200

    changes in 2 migration files

--- StartupMaps/blog/app/models/entity.rb
+++ StartupMaps/blog/app/models/entity.rb
@@ -1,3 +1,14 @@
 class Entity < ActiveRecord::Base
+	has_many :services 
+	has_many :startups
+	has_many :investors
+	has_many :enity_work_statuses
+	has_many :entity_work_protfolio 
+	has_many :entity_video_links
+	has_many :entity_social_links
+	has_many :entity_needs
+	has_many :enity_jobs
+	has_many :entity_goals
+	has_many :entity_available_internships
 end
 
--- StartupMaps/blog/app/models/send_merge_request.rb
+++ StartupMaps/blog/app/models/send_merge_request.rb
@@ -1,5 +1,5 @@
 class SendMergeRequest < ActiveRecord::Base
-	has_and_belongs_to_many :senders, :class_name => "Startup"
-	has_and_belongs_to_many :receivers, :class_name => "Startup"
+	has_and_belongs_to_many :senders, :class_name => "Startup" , :foreign_key => "sender_id"
+	has_and_belongs_to_many :receivers, :class_name => "Startup", :foreign_key => "receiver_id"
 	has_and_belongs_to_many :project
 end
