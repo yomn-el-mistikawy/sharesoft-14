164fd682455b02c211c561250e8c5cca9d3e2728
samarfikri <samar.alezaby@gmail.com>
2014-04-15 23:54:12 +0200

    This is the first trial of liking a comment

--- StartupMaps/blog/app/controllers/comments_controller.rb
+++ StartupMaps/blog/app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+
+  def  like_comment
+      @commentliked = Comments.find(params[:commentID])
+      @userid = GroupMembers.find(params[:memberID])
+      user = LikesOfComments.create(liker:userid, commentID: commentliked)
+
+  end
 end
