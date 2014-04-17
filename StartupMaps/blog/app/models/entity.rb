da9f46a3b6a56a179ad241df97d746d8fb01739c
sarah-fathallah <sfathallah.sf@gmail.com>
2014-04-17 09:24:50 +0200

    Requested columns added #153 & foreign key changes

--- StartupMaps/blog/app/models/entity.rb
+++ StartupMaps/blog/app/models/entity.rb
@@ -10,5 +10,9 @@ class Entity < ActiveRecord::Base
 	has_many :enity_jobs
 	has_many :entity_goals
 	has_many :entity_available_internships
+	has_many :senders, :through :group_invitation
+	has_many :receivers, :through :group_invitation
+	has_many :senders, :through :send_merge_request
+	has_many :receivers, :through :send_merge_request
 end
 
