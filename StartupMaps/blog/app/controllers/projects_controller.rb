c5137428bd3aa6fd3293b97decacd5f417df5ade
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-04-17 11:07:37 +0200

    edited_the_spacing_#55

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,34 +1,34 @@
 class ProjectsController < ApplicationController
 
-	# Defintion: This method takes the project_id and session id
-	# as input and calls get_suggested() to show a list of 
-	# projects with the same geographical location and
-	# category to the startup's opened project.The suggestions 
-	# are only shown for project owners. Moreover, if there are no 
-	# suggestions, a message is shown stating so.
-	# Input: project_id, entity_id
+  # Defintion: This method takes the project_id and session id
+  # as input and calls get_suggested() to show a list of 
+  # projects with the same geographical location and
+  # category to the startup's opened project.The suggestions 
+  # are only shown for project owners. Moreover, if there are no 
+  # suggestions, a message is shown stating so.
+  # Input: project_id, entity_id
 	# Output: project_id, suggested_project
 	# Author: Yomn El-Mistikawy
 
-	def suggest
-		if (StartupHaveProject.check_ownership(Project.find(params[:project_id]), Startup.find(session[:entity_id])).size != 0)
-			@suggested = Project.get_suggest(Project.find(params[:project_id]), Startup.find(session[:entity_id]))
-		else
-			render text: ""
-		end
-	end
+  def suggest
+    if (StartupHaveProject.check_ownership(Project.find(params[:project_id]), Startup.find(session[:entity_id])).size != 0)
+      @suggested = Project.get_suggest(Project.find(params[:project_id]), Startup.find(session[:entity_id]))
+    else
+      render text: ""
+    end
+  end
 
 
-	# Defintion: This method takes the suggested project id
-	# as input and views the project with a button for the startup
-	# to send a merge request to the project owner.
-	# Input: suggested_project
-	# Output: project_id, suggested_project
-	# Author: Yomn El-Mistikawy
+  # Defintion: This method takes the suggested project id
+  # as input and views the project with a button for the startup
+  # to send a merge request to the project owner.
+  # Input: suggested_project
+  # Output: project_id, suggested_project
+  # Author: Yomn El-Mistikawy
 
-	def show_suggested
-  	@project = Project.find(params[:suggested_project])
-	end
+  def show_suggested
+    @project = Project.find(params[:suggested_project])
+  end
 
 	  
 
--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -1,16 +1,16 @@
 class Project < ActiveRecord::Base
 	
-	# Defintion: This method takes a project as input
-	# and searches the projects table for projects of
-	# same geographical location and category and sends
-	# the results to the suggest function in the project
-	# controller.
-	# Input: project
-	# Output: a list of suggested projects
-	# Author: Yomn El-Mistikawy
+  # Defintion: This method takes a project as input
+  # and searches the projects table for projects of
+  # same geographical location and category and sends
+  # the results to the suggest function in the project
+  # controller.
+  # Input: project
+  # Output: a list of suggested projects
+  # Author: Yomn El-Mistikawy
 
-	def  self.get_suggest(project, startup)
-		@projects_owned_by_startup_ids = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
-		@suggested_projects = Project.where(:location => project.location, :category => project.category).where.not(:id => project.id, :id => @projects_owned_by_startup_ids)
-	end	
+  def  self.get_suggest(project, startup)
+    @projects_owned_by_startup_ids = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
+    @suggested_projects = Project.where(:location => project.location, :category => project.category).where.not(:id => project.id, :id => @projects_owned_by_startup_ids)
+  end	
 end
