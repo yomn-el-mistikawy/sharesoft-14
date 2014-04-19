3904cc29dcc79092210627725545c2791a3e423a
sarah-fathallah <sfathallah.sf@gmail.com>
2014-04-19 18:40:38 +0200

    Requested Migration files added

--- StartupMaps/blog/app/models/entity.rb
+++ StartupMaps/blog/app/models/entity.rb
@@ -2,17 +2,19 @@ class Entity < ActiveRecord::Base
 	has_many :services 
 	has_many :startups
 	has_many :investors
-	has_many :enity_work_statuses
-	has_many :entity_work_protfolio 
+	has_many :entity_work_statuses
+	has_many :entity_work_portfolio 
 	has_many :entity_video_links
 	has_many :entity_social_links
 	has_many :entity_needs
-	has_many :enity_jobs
+	has_many :entity_jobs
 	has_many :entity_goals
 	has_many :entity_available_internships
-	has_many :senders, :through :group_invitation
-	has_many :receivers, :through :group_invitation
-	has_many :senders, :through :send_merge_request
-	has_many :receivers, :through :send_merge_request
+	has_many :senders, :through => :group_invitations
+	has_many :receivers, :through => :group_invitations
+	has_many :senders, :through => :send_merge_requests
+	has_many :receivers, :through => :send_merge_requests
+	has_many :subscriber, :through => :subscrtipion
+	has_many :subscribee, :through => :subscrtipion
 end
 
--- StartupMaps/blog/app/models/entity_needs.rb
+++
@@ -1,3 +0,0 @@
-class EntityNeeds < ActiveRecord::Base
-	belongs_to :entity
-end
