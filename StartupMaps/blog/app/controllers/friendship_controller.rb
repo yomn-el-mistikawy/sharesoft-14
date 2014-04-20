c55517f7a683378448de490d24485e47809e7ec1
sarah-fathallah <sfathallah.sf@gmail.com>
2014-04-20 20:37:12 +0200

    View and new CRUD methods added #118

--- StartupMaps/blog/app/controllers/friendship_controller.rb
+++ StartupMaps/blog/app/controllers/friendship_controller.rb
@@ -1,19 +1,44 @@
 class FriendshipController < ApplicationController
-	def create
-  @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-  if @friendship.save
-    flash[:notice] = "Added friend."
-    redirect_to root_url
-  else
-    flash[:error] = "Unable to add friend."
-    redirect_to root_url
+	def index
+    @friends = current_user.friends
   end
-end
 
-def destroy
-  @friendship = current_user.friendships.find(params[:id])
-  @friendship.destroy
-  flash[:notice] = "Removed friendship."
-  redirect_to current_user
-end
+  def new
+    @users = User.all :conditions => ["id != ?", current_user.id]
+  end
+
+  def create
+    invitee = User.find_by_id(params[:user_id])
+    if current_user.invite invitee
+      redirect_to new_friend_path, :notice => "Successfully invited friend!"
+    else
+      redirect_to new_friend_path, :notice => "Sorry! You can't invite that user!"
+    end
+  end
+
+  def update
+    inviter = User.find_by_id(params[:id])
+    if current_user.approve inviter
+      redirect_to new_friend_path, :notice => "Successfully confirmed friend!"
+    else
+      redirect_to new_friend_path, :notice => "Sorry! Could not confirm friend!"
+    end
+  end
+
+  def requests
+    @pending_requests = current_user.pending_invited_by
+  end
+
+  def invites
+    @pending_invites = current_user.pending_invited
+  end
+
+  def destroy
+    user = User.find_by_id(params[:id])
+    if current_user.remove_friendship user
+      redirect_to friends_path, :notice => "Successfully removed friend!"
+    else
+      redirect_to friends_path, :notice => "Sorry, couldn't remove friend!"
+    end
+  end
 end
