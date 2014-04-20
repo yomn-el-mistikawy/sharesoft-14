50b5d4a50003ff93bf6d2e785c0524db17a5c315
mohamed <mohamed@ubuntu.(none)>
2014-04-20 14:50:03 -0700

    showing the sectors with markers on the map

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -1,2 +1,25 @@
 class MapsController < ApplicationController
+def create
+@entities_I = User.where(sector: params[:investors][:sectors])
+@entities_S = User.where(sector: params[:startups][:sectors])
+@entities_V = User.where(sector: params[:services][:sectors])
+
+
+@map = Gmaps4rails.build_markers(@entities_I) do |t, marker|
+        	marker.lat t.latitude
+        	marker.lng t.longitude
+        	marker.infowindow t.sector
+
+@map = Gmaps4rails.build_markers(@entities_S) do |t, marker|
+        	marker.lat t.latitude
+        	marker.lng t.longitude
+        	marker.infowindow t.sector
+
+@map = Gmaps4rails.build_markers(@entities_V) do |t, marker|
+        	marker.lat t.latitude
+        	marker.lng t.longitude
+        	marker.infowindow t.sector
+
+end
+
 end
