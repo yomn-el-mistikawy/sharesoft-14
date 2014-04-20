e47aa6942b8875326f1052a7c3dbaf23b986fad6
Ziad-abdelkader <ziad.abdelkader9@gmail.com>
2014-04-20 22:53:16 +0200

    Added briefly the button of subscribing but still more details to be added to the userstory as soon as possible

--- StartupMaps/blog/app/models/subscription.rb
+++ StartupMaps/blog/app/models/subscription.rb
@@ -2,3 +2,9 @@ class Subscription < ActiveRecord::Base
 	has_and_belongs_to_many :subscriber, :class_name => "entity" 
 	has_and_belongs_to_many :subscribee, :class_name => "entity"
 end
+
+
+
+ def show
+  	@profile= Profiles.all
+  end
