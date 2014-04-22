ddd1d08cb8895a9c72547071965ea1c8436bdcdd
OlaEnaba <ola.enaba@gmail.com>
2014-04-22 23:02:29 +0200

    modification after pulling new migration files

--- StartupMaps/blog/app/controllers/entities_controller.rb
+++ StartupMaps/blog/app/controllers/entities_controller.rb
@@ -2,6 +2,29 @@ class EntitiesController < ApplicationController
 
   before_action :set_entity, only: [:show, :edit, :update, :destroy]
 
+
+  # Definition: Finds the status that should be updated.
+  # Input: Status.
+  # Output: Updated Status.
+  # Author: Ola Enaba.
+ 
+  def  edit_status
+      @entity = Entity.find(session[:entity_id])
+  end
+
+
+  # Definition: Redirects to the new updated status.
+  # Input: Status.
+  # Output: Updated Status.
+  # Author: Ola Enaba.
+
+  def  update_status
+     @entity = Entity.find(session[:entity_id]) 
+     @work_status = EntityWorkStatus.create(:workstatus => params[:work_status]
+      , :entity_id => params[:entity_id])
+  end
+ 
+
   #Homepage of entities, which lists all created accounts
   # Adel Zee Badawy
   def index
@@ -99,40 +122,5 @@ class EntitiesController < ApplicationController
   end
  
 
-  # Definition: The user writes the status in the text box and click the button.
-  # Input: Status.
-  # Output: Updated Status.
-  # Author: Ola Enaba.
-  
-  def  update
-    @post = Post.find(params[1])
-    if @post.update(params[:post].permit(:title, :text))
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
-
-
-  # Definition: Finds the status that should be updated.
-  # Input: Status.
-  # Output: Updated Status.
-  # Author: Ola Enaba.
- 
-  def  editStatus
-      @entity = Entity.find(1)
-  end
-
-
-  # Definition: Redirects to the new updated status.
-  # Input: Status.
-  # Output: Updated Status.
-  # Author: Ola Enaba.
-
-  def  updateStatus
-     @entity = Entity.find(1) 
-       @entity.update(params[:entity].permit(:work_status))
-       redirect_to entity_editStatus_path
-  end
 end
 
