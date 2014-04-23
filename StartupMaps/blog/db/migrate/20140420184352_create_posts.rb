8e3a56732973fac44adcb6826a692ec51248c51a
Sarah Fathallah <sfathallah.sf@gmail.com>
2014-04-23 21:20:02 +0200

    Update 20140420184352_create_posts.rb

--- StartupMaps/blog/db/migrate/20140420184352_create_posts.rb
+++ StartupMaps/blog/db/migrate/20140420184352_create_posts.rb
@@ -1,12 +1,3 @@
 class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :title
-      t.string :text
-      t.belongs_to :group
-      t.belongs_to :startup
 
-      t.timestamps
-    end
-  end
 end
