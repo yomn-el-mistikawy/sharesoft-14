2c88c552d006a8536db7da66497e4ee00d241d46
Sarah Fathallah <sfathallah.sf@gmail.com>
2014-04-23 21:21:22 +0200

    Update 20140420190647_create_comments.rb

--- StartupMaps/blog/db/migrate/20140420190647_create_comments.rb
+++ StartupMaps/blog/db/migrate/20140420190647_create_comments.rb
@@ -1,12 +1,3 @@
 class CreateComments < ActiveRecord::Migration
-  def change
-    create_table :comments do |t|
-      t.string :commenter
-      t.string :comment
-      t.belongs_to :post
-      t.belongs_to :startup
 
-      t.timestamps
-    end
-  end
 end
