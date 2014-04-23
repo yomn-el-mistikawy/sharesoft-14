fe8135a9305dce62c1fece277a0d97abd3d443af
Sarah Fathallah <sfathallah.sf@gmail.com>
2014-04-23 21:22:37 +0200

    Update 20140422203611_create_likes.rb

--- StartupMaps/blog/db/migrate/20140422203611_create_likes.rb
+++ StartupMaps/blog/db/migrate/20140422203611_create_likes.rb
@@ -1,11 +1,3 @@
 class CreateLikes < ActiveRecord::Migration
-  def change
-    create_table :likes do |t|
-      t.string :liker
-      t.belongs_to :startup
-      t.belongs_to :likable, :polymorphic => true
 
-      t.timestamps
-    end
-  end
 end
