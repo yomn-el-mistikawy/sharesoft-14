92292e98e8dd475de57229c0a0d6691e1b3cc495
Hana Magdy <hana.hosny94@gmail.com>
2014-04-24 18:05:59 +0200

    please find the mini amendments in show and project controller

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -51,22 +51,22 @@ class ProjectsController < ApplicationController
   # == End  == 
   
 
-=begin
-	This methods queries the table project for projects that are in the same category and location as the current project
-	Input: selected project id
-	Output: @suggested
-	Author: Yomn El-Mistikawy
-=end
+  #  =begin
+	# This methods queries the table project for projects that are in the same category and location as the current project
+	# Input: selected project id
+	# Output: @suggested
+	# Author: Yomn El-Mistikawy
+  #  =end
 def suggest
 	@project= Project.find(1)
     @suggested= Project.where(:location => "cairo", :category => "baking").where.not(:id => "1", :startup_id => "1")
 end
-=begin
-	This method shows the profile of the selected suggested project
-	Input: selected project id
-	Output: view showing the profile of the project with a merge request button
-	Author: Yomn El-Mistikawy
-=end
+  #  =begin
+	# This method shows the profile of the selected suggested project
+	# Input: selected project id
+	# Output: view showing the profile of the project with a merge request button
+	# Author: Yomn El-Mistikawy
+  #  =end
 def showSuggested
   @project= Project.find(params[:project])
 end
