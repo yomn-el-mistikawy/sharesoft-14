879e913ad7781da64c3fa717429f360942c1c51f
Hana Magdy <hana.hosny94@gmail.com>
2014-04-24 02:59:25 +0200

    Please find the semi-latest update for Launching a project

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,33 +1,16 @@
 class ProjectsController < ApplicationController
 
-# == Begin  == 
-# UserStory: A startup can see a list of his projects. 
-# Definition: This method allows you to get a list of projects. 
-# that belong to a specific startup.
-# Action: index.
-# Author: Hana Magdy.
 
-  def index
-    session[:entity_id] = 1
-    @projects = Project.listing_projects(Startup.find(session[:entity_id]))
-    # @projects = Project.all
-    # @startup = Startup.find(session[:entity_id])
-    # @projects = @startup.project.find( :all)
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-
-# UserStory: As a startup, I can set a project goal, milestone,
-# requirements (roles, resources).
-# Definition: This method shows all details of a project. 
-# that belong to a specific startup and is linked to the show HTML file.
-# Input: Project_id.
-# Output: Project_id "all project description".
-# Action: show.
-# Author: Hana Magdy.
+  # Definition: "As a startup, I can set a project goal, milestone,
+  # requirements (roles, resources) "
+  # This method shows all details of a project
+  # that belong to a specific startup and is linked to the show HTML file
+  # which also includes the launch and editing part
+  # respond_to --> gives a direct access to the HTML/XML/PDF whatever is it
+  # it's reachable and knows what's happening in the file.
+  # Input: Project_id.
+  # Output: Project_id "all project description".
+  # Author: Hana Magdy.
 
   def show
     @project = Project.find(params[:id])
@@ -38,44 +21,16 @@ class ProjectsController < ApplicationController
   end
 
 
-# UserStory: As a  startup, I can set a project goal, milestone, requirements (roles, resources)
-# Definition: This method allows you to edit a project given its project's id.
-# Input: Project_id.
-# Output: Project_id "all project description".
-# Action: edit.
-# Author: Hana Magdy.
 
-  def edit
-    @project = Project.find(params[:id])
-  end
-
-
-# UserStory: As a  startup, I can set a project goal, milestone, requirements (roles, resources).
-# Definition: Allows editing of project details, specifically goals, milestones and requirements.
-# Redirects user to project's page (show project) on success.
-# Re-renders the edit project page on error.
-# Action: update.
-# Author: Hana Magdy.
-
-  def update
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project].permit(:goals, :milestones, :requirements))
-        format.html { redirect_to @project, notice: "Successfully updated project" }
-      else
-        format.html { render :edit }
-      end
-    end
-
-  end
-
-
-  # UserStory: A project is launched after all requirements are met.
-  # Definition:Changes the status of a project and redirects to the project's 
-  #page (show project) on success or error.
-  # with the exception of displaying a success/error message.
-  # Action: change_launch_status.
+  # Definition: "A project is launched after all requirements are met"
+  # Changes the status of a project and redirects to the project's 
+  # page (show project) on success or error
+  # with the exception of displaying a success/error message
+  # update_attribute --> updates the rows
+  # update the status of launch project from unlaunch to launched and vice versa.
+  # Input: project_id. "on the show page".
+  # Output: Void "it's an action" returns the success of the
+  # changeable button of launch upon of it's previous status. 
   # Author: Hana Magdy.
 
   def change_launch_status
--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -1,4 +1,16 @@
 class Project < ActiveRecord::Base
-	belongs_to :startup
+	belongs_to :entity
 	
+  # Definition: "A project is launched after all requirements are met"
+  # Changes the status of a project and redirects to the project's 
+  # page (show project) on success or error
+  # update the status of launch project from unlaunch to launched and vice versa.
+  # Input: Void.
+  # Output: Void "it's an action" returns the success 
+  # of the changeable button of launch upon of it's previous status. 
+  # Author: Hana Magdy.
+
+	def launched?
+		launch ? "Yes" : "No"
+	end	
 end
