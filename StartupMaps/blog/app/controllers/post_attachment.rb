45c91c36a4751f526c647469348dd68d04eecd46
essama <essamazzam5@gmail.com>
2014-04-24 14:24:55 +0200

    Update post_attachment.rb

--- StartupMaps/blog/app/controllers/post_attachment.rb
+++ StartupMaps/blog/app/controllers/post_attachment.rb
@@ -1,33 +1,27 @@
 class PostAttachment < ApplicationController
   
-
   # Definition: Lists all the uploaded resumes on starting up
   # Input: none
   # Output: list of resumes
   # Author: Essam Azzam
 
-
   def index
     @resumes = Resume.all
   end
 
-  
   # Definition: Initiate a new resume variable 
   # Input: none
   # Output: resume 
   # Author: Essam Azzam
 
-
   def new
     @resume = Resume.new
   end
   
-
   # Definition: Creates a record in the database with the given parameters
   # Input: name, attachment
   # Output: void
   # Author: Essam Azzam
-
 
   def create
    @resume = Resume.new(resume_params)
@@ -38,12 +32,10 @@ class PostAttachment < ApplicationController
     end
   end
 
-
   # Definition: Deletes a record from the database
   # Input: resume
   # Output: void
   # Author: Essam Azzam
-
 
   def destroy
    @resume = Resume.find(params[:id])
@@ -51,12 +43,10 @@ class PostAttachment < ApplicationController
    redirect_to resumes_path, notice:  "The resume #{@resume.name} has been deleted."
   end
 
-
   # Definition: Returns the parameters of the resume
   # Input: resume
   # Output: name, attachment
   # Author: Essam Azzam
-
 
   private
    def resume_params
