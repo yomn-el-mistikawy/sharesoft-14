090a156756b4271eada8b8d983f30c618075549f
sarah-fathallah <sfathallah.sf@gmail.com>
2014-04-24 18:23:46 +0200

    Friendship controller added with its documentation

--- StartupMaps/blog/app/controllers/friendship_controller.rb
+++ StartupMaps/blog/app/controllers/friendship_controller.rb
@@ -1,3 +1,58 @@
 class FriendshipController < ApplicationController
+  
 
+  # Definition: searches for the desired user id, and saves it. The method also calls the method request 
+  # from the model friend ship to update the database.
+  # Input: void 
+  # Output: void
+  # Authour: Sarah Fathallah
+
+  def create
+    @user = User.find(params[:friendship][:friend_id])
+    Friendship.request(current_user, @user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+  
+
+  # Definition: checks if there is a request and calls the accept method from the model if the accept button is 
+  # clicked. 
+  # Input: void
+  # Output: void
+  # Authour: Sarah Fathallah
+
+  def update
+    @user = Friendship.find_by_id(params[:id]).friend
+    if current_user.requested_friends.include?(@user)
+      Friendship.accept(current_user, @user)
+    end
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+
+  # Definition: checks if there is a request and calls the breakup method from the model to remove the friendship if 
+  # it is pending, requested or already done.
+  # Input: void
+  # Output: void
+  # Authour: Sarah Fathallah
+
+  def destroy
+    @user = Friendship.find_by_id(params[:id]).friend
+    if current_user.requested_friends.include?(@user)
+      Friendship.breakup(current_user, @user)
+    elsif current_user.pending_friends.include?(@user)
+      Friendship.breakup(current_user, @user)
+    elsif current_user.friends.include?(@user)
+      Friendship.breakup(current_user, @user)
+    end
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
 end
