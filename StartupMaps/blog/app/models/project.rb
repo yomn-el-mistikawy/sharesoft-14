e8c21fdc9ac98540f31a6ecf54c544e5557757cd
Hana Magdy <hana.hosny94@gmail.com>
2014-04-24 02:49:04 +0200

    please find a mini change in this branch, project's model

--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -1,28 +1,4 @@
 class Project < ActiveRecord::Base
 	belongs_to :startup
-
-#UserStory: A project is launched after all requirements are met.
-#Definition: This method is a boolean method that changes the projected. 
-# from unlaunched to launched and vice versa.
-# Action: launched?.
-# Author: Hana Magdy.
-
-	def launched?
-		launch ? "Yes" : "No"
-	end	
-
-#UserStory: A startup can see a list of his projects. 
-#Definition: This method takes the startups as input. 
-# and views all the projects related to this startup.
-# Input: Project_id, startup_id.
-# Output: Project_id "all project description". 
-# Action: listing_projects.
-# Author: Hana Magdy.
-	
-	def  self.listing_projects(startup)
-		@startups_listing_projects = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
-		Project.where(:id => @startups_listing_projects)
-
-	end
 	
 end
