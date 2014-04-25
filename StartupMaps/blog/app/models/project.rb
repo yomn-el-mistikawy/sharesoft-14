9287a88d31e96185ee72b0008bec4a0ff7132888
amrgamal18 <amrjimmy18@hotmail.com>
2014-04-25 14:21:02 +0200

    validation added for failure senarios

--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -14,5 +14,15 @@ class Project < ActiveRecord::Base
     @projects_owned_by_startup_ids = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
     @suggested_projects = Project.where(:location => project.location, :category => project.category).where.not(:id => project.id, :id => @projects_owned_by_startup_ids)
   end	
+
+  # Definition: VAlidation for unique name and presence of all values
+  # Input: No inpput
+  # output: No output
+  # Author: Amr Gamal
+  validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_presence_of :location
+  validates_presence_of :category
+  validates_presence_of :description
 end
 
