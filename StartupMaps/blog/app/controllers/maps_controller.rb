46bc95e1f0811cef5cd837685e10f1c0188d617f
Mohamed1234 <sharawyMido@gmail.com>
2014-04-25 14:39:33 -0700

    documentation and implementation convention #71

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -1,39 +1,39 @@
 class MapsController < ApplicationController
  
- #Definition: this action show the names of startups ,investors and the sevices that are in 
+ #Definition: This action show the names of startups ,investors and the sevices that are in 
     #sectors agriculture,manufacturing,trading,clothing,telecommunications on the map 
     #and buid markers according to the sectors.
-#Input:sector.
-#Output: name.
-#Author: mohamed sharawy. 
+#Input:Sector.
+#Output: Name.
+#Author: Mohamed sharawy. 
 
 
   def  create
-    @entities = Startup.where( sector: params[:entities][:sectors] )
-    @entities_I = Investor.where( sector: params[:entities][:sectors] )
-    @entities_V = Service.where( sector: params[:entities][:sectors] )
+    @entities_startups = Startup.where( sector: params[:entities][:sectors] )
+    @entities_investors = Investor.where( sector: params[:entities][:sectors] )
+    @entities_services = Service.where( sector: params[:entities][:sectors] )
 
 
-    @map = Gmaps4rails.build_markers( @entities ) do |t, marker|
+    @startups_markers = Gmaps4rails.build_markers( @entities_startups ) do |t, marker|
       marker.lat t.latitude
       marker.lng t.longitude
       marker.infowindow t.sector
-  end
+    end
 
-    @map_I = Gmaps4rails.build_markers( @entities_I ) do |t, marker|
+    @investor_markers = Gmaps4rails.build_markers( @entities_investors ) do |t, marker|
       marker.lat t.latitude
       marker.lng t.longitude
       marker.infowindow t.sector
-  end
+    end
 
-    @map_V = Gmaps4rails.build_markers( @entities_V ) do |t, marker|
+    @services_markers = Gmaps4rails.build_markers( @entities_services ) do |t, marker|
       marker.lat t.latitude
       marker.lng t.longitude
       marker.infowindow t.sector
-  end
+    end
 
   end
-  end
+end
 
 
 
