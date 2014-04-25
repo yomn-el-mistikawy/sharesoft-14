acc0069494d02b277854ad8cf3f941f379e2510a
samarfikri <samar.alezaby@gmail.com>
2014-04-25 21:21:27 +0200

    documentation and implementation modified :)

--- StartupMaps/blog/app/controllers/comments_controller.rb
+++ StartupMaps/blog/app/controllers/comments_controller.rb
@@ -1,9 +1,3 @@
 class CommentsController < ApplicationController
 
-  def  like_comment
-      @commentliked = Comments.find(params[:commentID])
-      @userid = GroupMembers.find(params[:memberID])
-      user = LikesOfComments.create(liker:userid, commentID: commentliked)
-
-  end
 end
--- StartupMaps/blog/app/controllers/likes_controller.rb
+++ StartupMaps/blog/app/controllers/likes_controller.rb
@@ -1,12 +1,11 @@
 class LikesController < ApplicationController
-  
-# Defintion: This method enables a group member to like 
-# a post or a comment. It takes as input the likable id
-# and type and the liker id and the current location to
-# redirect back to it and it puts the like in the database.
-# Input: likable, id, type, currentaction, currentcontrol.
-# Output: None.
-# Author: Samar Fikri.
+  # Defintion: This method enables a group member to like 
+  # a post or a comment. It takes as input the likable id
+  # and type and the liker id and the current location to
+  # redirect back to it and it puts the like in the database.
+  # Input: likable, id, type, currentaction, currentcontrol.
+  # Output: None.
+  # Author: Samar Fikri.
 
   def like
 
@@ -22,22 +21,22 @@ class LikesController < ApplicationController
     @new.likable_type = @type
     @new.save
 
-     redirect_to :action=> @currentAction , :controller => @currentontrol
-     return
-#<a href="../likes/like?currentcontrol=comment&currentaction=index&type=comment&likable=<%= comment.id%> >dasdsa</a>
-#localhost:3000/likes/like?currentcontrol=projects&currentaction=new&type=comment&likable=1&id=2
+    redirect_to :action=> @currentAction , :controller => @currentontrol
+    return
+  #<a href="../likes/like?currentcontrol=comment&currentaction=index&type=comment&likable=<%= comment.id%> >dasdsa</a>
+  #localhost:3000/likes/like?currentcontrol=projects&currentaction=new&type=comment&likable=1&id=2
   end
 
 
-# Definition: This method just gets the contents of the 
-# likes database and groups the similar objects together.
-# Inputs: none.
-# Outputs: all likes in the database, grouped by the id.
-# of the post or comment.
-# Author: Samar Fikri.
+  # Definition: This method just gets the contents of the 
+  # likes database and groups the similar objects together.
+  # Inputs: none.
+  # Outputs: all likes in the database, grouped by the id.
+  # of the post or comment.
+  # Author: Samar Fikri.
 
   def showposts
-           @lik = Like.find(:all).group_by(&:likable_id)
+    @lik = Like.find(:all).group_by(&:likable_id)
   end
 
 end
