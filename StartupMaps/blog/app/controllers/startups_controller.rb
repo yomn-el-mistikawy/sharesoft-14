692bcc5db969947b2f0840a4e67d3b663c9e520a
maha <maha@ubuntu.(none)>
2014-04-25 12:25:51 +0100

    done_with_adding_startups_to_geographic_groups_#58

--- StartupMaps/blog/app/controllers/startups_controller.rb
+++ StartupMaps/blog/app/controllers/startups_controller.rb
@@ -1,2 +1,22 @@
 class StartupsController < ApplicationController
+
+	def create
+		@newgeo_id = create_geo_group(startup_params[:location])
+		create_groups_startups(startup_params[:name], @newgeo_id)
+ 	end
+
+    def update
+    	if @startup.update_attribute(:location, @startup.location)
+      		@newgeo_id = create_geo_group(startup_params[:location])
+      		create_groups_startups(startup_params[:name], @newgeo_id)
+    	end
+	end
+
+	def destroy 
+    	@location = @startup.location
+    	@id = @startup.id
+    	destroy_groups_startups(@id)
+    	destroy_geo_group(@location)
+ 	end
+
 end
--- StartupMaps/blog/config/routes.rb
+++ StartupMaps/blog/config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
   resources :group_members
 
+  resources :groups_startups
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :projects do
