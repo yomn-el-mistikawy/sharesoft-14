6b843a5203d2a850b9d09eab35ae08eb35e17995
hebaabdelfattah <heba.azak@gmail.com>
2014-04-25 20:00:47 +0200

    edited implementation

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -9,14 +9,14 @@ class MapsController < ApplicationController
 	def show_merged
 		@merged = Startup.where(:joint_ventures != "")
 		@h = Gmaps4rails.build_markers(@merged) do |t, marker|
-      marker.lat t.latitude
-      marker.lng t.longitude
-      marker.infowindow t.joint_ventures
-      marker.picture({
-        "url" => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-265cb2/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/foodcan.png",
-        "width"   => 32,
-        "height"  => 32
-        })
-      end
+    marker.lat t.latitude
+    marker.lng t.longitude
+    marker.infowindow t.joint_ventures
+    marker.picture({
+      "url" => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-265cb2/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/foodcan.png",
+      "width"   => 32,
+      "height"  => 32
+      })
+    end
 	end
 end
