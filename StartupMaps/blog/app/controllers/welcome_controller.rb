7b29b68e8752c902ab53565256c71aeab5b0a160
AdelZee <adel.zee93@gmail.com>
2014-04-25 13:23:13 +0200

    Fixed documentation errors. #59

--- StartupMaps/blog/app/controllers/welcome_controller.rb
+++ StartupMaps/blog/app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
-#Leads to the index page of welcome
-# Adel Zee Badawy
-  def index
-  end
+	# Definition: Leads to the index page of welcome.
+	# Input: Void
+	# Output: Void
+	# Author: Adel Zee Badawy
+	def index
+	end
 end
 
