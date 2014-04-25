bde93afc23023ca59519ec00ff884064b0481f21
hebaabdelfattah <heba.azak@gmail.com>
2014-04-25 23:24:05 +0200

    edited implementation

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -6,17 +6,17 @@ class MapsController < ApplicationController
 # Output: List of merged entities on the map with markers.
 # Author: Heba Abdelfattah.
 
-	def show_merged
-		@merged = Startup.where(:joint_ventures != "")
-		@h = Gmaps4rails.build_markers(@merged) do |t, marker|
-    marker.lat t.latitude
-    marker.lng t.longitude
-    marker.infowindow t.joint_ventures
-    marker.picture({
-      "url" => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-265cb2/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/foodcan.png",
-      "width"   => 32,
-      "height"  => 32
-      })
-    end
+  def show_merged
+	  @merged = Startup.where(:joint_ventures != "")
+	  @h = Gmaps4rails.build_markers(@merged) do |t, marker|
+      marker.lat t.latitude
+      marker.lng t.longitude
+      marker.infowindow t.joint_ventures
+      marker.picture({
+        "url" => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-265cb2/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/foodcan.png",
+        "width"   => 32,
+        "height"  => 32
+        })
+      end
 	end
 end
