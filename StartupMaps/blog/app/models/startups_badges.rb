245619bc0c9c2962fddb655e9603d8d13f63bb91
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-05-03 19:04:27 +0200

    Finished set_badge_collector_badges #217

--- StartupMaps/blog/app/models/startups_badges.rb
+++ StartupMaps/blog/app/models/startups_badges.rb
@@ -9,7 +9,9 @@ class StartupsBadges < ActiveRecord::Base
   # Author: Yomn El-Mistikawy
 
   def self.set_badges(startup_id)
-    recently_achieved_badges = StartupsBadges.set_year_badges(startup_id)
+    recently_achieved_badges = []
+    recently_achieved_badges = recently_achieved_badges + StartupsBadges.set_year_badges(startup_id)
+    recently_achieved_badges = recently_achieved_badges + StartupsBadges.set_badge_collection_badges(startup_id)
     return recently_achieved_badges
 	end
 
@@ -53,7 +55,56 @@ class StartupsBadges < ActiveRecord::Base
   def self.set_launch_badges(startup_id)
   end 
 
+
+  # Definition: This method takes the startup_id and gets the unachieved badges. If the startup has
+  # achieved all of the badges in a certain category, then a platinum badge is given. Moreover,
+  # if all badges are collected, a platinum badge is given.
+  # Input: startup_id
+  # Output: recently_achieved_badges
+  # Author: Yomn El-Mistikawy
+
   def self.set_badge_collection_badges(startup_id)
+    unachieved_badges = StartupsBadges.get_achieved_unachieved_badges(startup_id, 0)
+    achieved_badges = StartupsBadges.get_achieved_unachieved_badges(startup_id, 1)
+    recently_achieved_badges = []
+    if (unachieved_badges.where(:id => "16").size == 1)
+      if (achieved_badges.where(:category => "year").size == 3)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 16)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(16).description]
+      end
+    end 
+    if (unachieved_badges.where(:id => "17").size == 1)
+      if (achieved_badges.where(:category => "view").size == 3)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 17)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(17).description]
+      end
+    end 
+    if (unachieved_badges.where(:id => "18").size == 1)
+      if (achieved_badges.where(:category => "requirements").size == 3)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 18)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(18).description]
+      end
+    end 
+    if (unachieved_badges.where(:id => "19").size == 1)
+      if (achieved_badges.where(:category => "targets").size == 3)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 19)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(19).description]
+      end
+    end  
+    if (unachieved_badges.where(:id => "20").size == 1)
+      if (achieved_badges.where(:category => "launched").size == 3)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 20)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(20).description]
+      end
+    end  
+    if (unachieved_badges.where(:id => "21").size == 1)
+      achieved_badges = StartupsBadges.get_achieved_unachieved_badges(startup_id, 1)
+      if (achieved_badges.where(:category => "collector").size == 5)
+        StartupsBadges.create(:startup_id => startup_id, :badge_id => 21)
+        recently_achieved_badges = recently_achieved_badges + [Badge.find(21).description]
+      end
+    end
+    return recently_achieved_badges
   end 
 	
 	
