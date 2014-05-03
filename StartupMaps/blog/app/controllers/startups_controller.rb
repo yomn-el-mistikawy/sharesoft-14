ac352a3a03ab0ca1ff4b62f64aabb0b9baa892a2
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-05-03 19:24:58 +0200

    Added_the_code_for_show_and_show_unachieved_badges #217

--- StartupMaps/blog/app/controllers/startups_controller.rb
+++ StartupMaps/blog/app/controllers/startups_controller.rb
@@ -1,4 +1,5 @@
 class StartupsController < ApplicationController
+  
   # Definition: When a startup opens its profile, if new badges is completed,
   # then a message appears showing the new badges achieved. A button will be available
   # to give the owner the option to view the unachieved badges. Moreover, all the
@@ -8,10 +9,22 @@ class StartupsController < ApplicationController
   # Author: Yomn El-Mistikawy
 
   def show
+  	session[:entity_id] = 3
     startup = Startup.find(params[:id])
       if session[:entity_id] == startup.entity_id
         @badges_just_achieved_description = StartupsBadges.set_badges(params[:id])
       end	
     @achieved_badges = StartupsBadges.get_achieved_unachieved_badges(params[:id], 1)
-  end	
+  end
+
+
+  # Definition: This is a pop-up page that shows a list of all the unachieved badges.
+  # The button directing to it only appears to the profile owner.
+  # Input: startup_id, session_id
+  # Output: void
+  # Author: Yomn El-Mistikawy	
+
+  def show_unachieved_badges
+    @unachieved_badges = StartupsBadges.get_achieved_unachieved_badges(params[:startup_id], 0)
+  end  
 end
