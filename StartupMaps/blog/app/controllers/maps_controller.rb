d88ff7a4b48ab9686b6a3100bf5925a290a13d87
Alia Tarek <alia@ubuntu.(none)>
2014-05-04 20:26:35 +0200

    show map

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -1,2 +1,15 @@
 class MapsController < ApplicationController
+	def  show_startups
+    @startups = Test.all
+    @markers = Gmaps4rails.build_markers(@startups) do |t, marker| 
+      marker.lat t.latitude
+      marker.lng t.longitude 
+      marker.infowindow t.joint_ventures 
+      marker.picture({  
+        "url" => "http://rideforclimate.com/nukes/all/images/blue.png",
+        "width" => 32, 
+        "height" => 32})
+    end
+    render :template => 'maps/index'
+  end
 end
