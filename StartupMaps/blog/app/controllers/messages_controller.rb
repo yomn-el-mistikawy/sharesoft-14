6e6a218b480c33c5e6283095b0af06686f327fca
sarah-fathallah <sfathallah.sf@gmail.com>
2014-05-05 20:50:05 +0200

    Message controller and helper done, crud methods added #237

---
+++ StartupMaps/blog/app/controllers/messages_controller.rb
@@ -0,0 +1,52 @@
+class MessagesController < ApplicationController
+
+  def index
+    @entity = current_entity
+    @messages = Message.order("created_at desc")
+    respond_to do |format|
+      format.html
+      format.json { render json: @messages }
+    end
+  end
+
+  def show 
+    @message = Message.find(params[:id])
+    @entity = current_entity
+    if (@entity.email == @message.sender) || (@entity.email == @message.recepient)
+    else
+      respond_to do |format|
+        format.html { redirect_to :action => :index, notice: 'No message found' }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(params[:message])
+    @message.sender = current_entity.email
+    @message.save
+    
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to :action => :index, notice: 'Message has been sent.' }
+        format.json { render json: @messages }
+      else
+        format.html { redirect_to :action => :new, notice: 'Error: Please try again.' }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { head :no_content }
+    end
+  end
+end
