0164b864fbcee807d8aeb224f5f823fdc7eccc55
Haya El-Menawy <hayaelmenawy@Hayas-MacBook-Air.local>
2014-05-05 17:02:26 +0200

    changes made in entity model

--- StartupMaps/blog/app/models/entity.rb
+++ StartupMaps/blog/app/models/entity.rb
@@ -1,4 +1,8 @@
 class Entity < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :services 
 	has_many :startups
 	has_many :investors
