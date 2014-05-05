2d6466c94f68c494902d072b1f9862dde7fb30c6
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-05-05 19:03:41 +0200

    Made_changes_needed_for_moving_to_entities_controller #217

--- StartupMaps/blog/app/controllers/entities_controller.rb
+++ StartupMaps/blog/app/controllers/entities_controller.rb
@@ -1,97 +1,31 @@
 class EntitiesController < ApplicationController
-  before_action :set_entity, only: [:show, :edit, :update, :destroy]
 
-  #Homepage of entities, which lists all created accounts
-  # Adel Zee Badawy
-  def index
-    @entities = Entity.all
-  end
+ # Definition: When a startup opens its profile, if new badges is completed,
+ # then a message appears showing the new badges achieved. A button will be available
+ # to give the owner the option to view the unachieved badges. Moreover, all the
+ # achieved badges with highest level are shown for to users viewing the profile.
+ # Input: startup_id, session_id
+ # Output: void
+ # Author: Yomn El-Mistikawy
 
- #Leads to the show webpages which views created accounts
- # Adel Zee Badawy
-  def show
-  end
-
-  #Gives access to new accounts
-  # Adel Zee Badawy
-  def new
-    @account = Entity.new
-  end
-
+ def show
+  #session[:entity_id] = 2
+  # if session[:entity_id] == params[:id]
+  @badges_just_achieved_description = StartupsBadges.set_badges(params[:id])
+  # end 
+  @achieved_badges = StartupsBadges.get_achieved_unachieved_badges(params[:id], 1, 0,1)
+  @all_achieved_badges = StartupsBadges.get_achieved_unachieved_badges(params[:id], 1, 1,1)
+ end
 
 
- #Creates accounts
- # Adel Zee Badawy
-  def create
-    @entity = Entity.new(entity_params)
+ # Definition: This is a pop-up page that shows a list of all the unachieved badges.
+ # The button directing to it only appears to the profile owner.
+ # Input: startup_id, session_id
+ # Output: void
+ # Author: Yomn El-Mistikawy 
 
-    respond_to do |format|
-      if @entity.save
-        format.html { redirect_to @entity, notice: 'Entity was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @entity }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  
-  def update
-    respond_to do |format|
-      if @entity.update(entity_params)
-        format.html { redirect_to @entity, notice: 'Entity was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
- #Deletes created accounts
- # Adel Zee Badawy
-  def destroy
-    @entity.destroy
-    respond_to do |format|
-      format.html { redirect_to entities_url }
-      format.json { head :no_content }
-    end
-  end
-#Private disallows the view to use the methods inside, but lets methods in the same controller to use the methods.
-# Adel Zee Badawy
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    # Adel Zee Badawy
-    def set_entity
-      @entity = Entity.find(params[:id])
-    end
-
-    # Allows the method to read the inputs
-    # Adel Zee Badawy
-    def entity_params
-      params.require(:entity).permit(:name, :email, :password, :availability)
-    end
-
-  def friends
-    @title = "Friends"
-    @user = User.find(params[:id])
-    @users = @user.friends.paginate(:page => params[:page])
-    render 'show_friends'
-  end
-
-  def pending_friends
-    @title = "Pending friends"
-    @user = User.find(params[:id])
-    @users = @user.pending_friends.paginate(:page => params[:page])
-    render 'show_friends'
-  end
-
-  def requested_friends
-    @title = "Requested friends"
-    @user = User.find(params[:id])
-    @users = @user.requested_friends.paginate(:page => params[:page])
-    render 'show_friends'
-  end
+ def show_unachieved_badges
+  @unachieved_badges = StartupsBadges.get_achieved_unachieved_badges(params[:entity_id], 0, 0, 1)
+  end  
 end
 
