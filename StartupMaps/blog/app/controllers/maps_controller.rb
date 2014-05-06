70d51d1a4a7e4f50fcba70a3e4adc80b82e2e114
Alia Tarek <alia@ubuntu.(none)>
2014-05-06 16:49:06 +0200

    #218_homepage button redirecting to map

--- StartupMaps/blog/app/controllers/maps_controller.rb
+++ StartupMaps/blog/app/controllers/maps_controller.rb
@@ -1,15 +1,6 @@
 class MapsController < ApplicationController
-	def  show_startups
-    @startups = Test.all
-    @markers = Gmaps4rails.build_markers(@startups) do |t, marker| 
-      marker.lat t.latitude
-      marker.lng t.longitude 
-      marker.infowindow t.joint_ventures 
-      marker.picture({  
-        "url" => "http://rideforclimate.com/nukes/all/images/blue.png",
-        "width" => 32, 
-        "height" => 32})
-    end
+	def  create
+  
     render :template => 'maps/index'
   end
 end
