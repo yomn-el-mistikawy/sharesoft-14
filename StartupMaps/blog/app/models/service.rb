da0cae1c7bd4cf7b24c1d5645dacc6b822e0da7a
Mozdan Ahmed <mozdan.hamd@gmail.com>
2014-05-06 23:39:48 +0300

    #240_This_is_the_basic_functionality_of_the_search_engine

--- StartupMaps/blog/app/models/service.rb
+++ StartupMaps/blog/app/models/service.rb
@@ -1,3 +1,8 @@
 class Service < ActiveRecord::Base
 	belongs_to :Entity
+
+	 def self.search_service(search)
+    @service= Service.find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+ 
+   end 
 end
