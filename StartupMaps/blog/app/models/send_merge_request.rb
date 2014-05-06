5a66d41308de7c4363753d319152346128eff4bc
Ziad-abdelkader <ziad.abdelkader9@gmail.com>
2014-05-06 10:47:25 +0200

    added a button to send merge requests which will enter the entries in the table

--- StartupMaps/blog/app/models/send_merge_request.rb
+++ StartupMaps/blog/app/models/send_merge_request.rb
@@ -2,4 +2,15 @@ class SendMergeRequest < ActiveRecord::Base
 	has_and_belongs_to_many :senders, :class_name => "startup" 
 	has_and_belongs_to_many :receivers, :class_name => "startup"
 	has_and_belongs_to_many :project
+
+
+
+	 def merge_request
+     	
+ 		@suggested_project = Project.find(params[:suggested_project])
+ 		@project = Project.find(params[:project_id])
+        @send_request = SendMergeRequest.create(:sender_id => session_id, :receiver_id => @suggested_project.startup_id)
+ 	 end
+  
+
 end
