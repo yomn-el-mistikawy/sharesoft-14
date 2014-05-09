ad636370555b949bb70191cae0fb78a222821c39
yomn-el-mistikawy <yomn.t.mistik@gmail.com>
2014-05-09 09:54:05 +0200

    Trying_to_test_year_badges #217

--- StartupMaps/blog/spec/models/startups_badges_spec.rb
+++ StartupMaps/blog/spec/models/startups_badges_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe StartupsBadges do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Sets achieved number of years badges" do
+    entity = Entity.create!(id: "1", name: "Yomn", username: "Yomn", email: "yomn@gmail.com")
+    entity.type = "Startup"
+    startup = Startup.create!(id: "1", number_of_working_years: "6", entity_id: entity.id)
+
+    expect(StartupsBadges.set_badges(entity.id)).to eq([Badge.find(1),Badge.find(2), Badge.find(3), Badge.find(19)])
+  end
 end
