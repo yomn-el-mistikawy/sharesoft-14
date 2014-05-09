225943f7ace052987aa0236fbc24e9fe5c02d7f7
Haya El-Menawy <hayaelmenawy@Hayas-MacBook-Air.local>
2014-05-09 17:46:44 +0200

    Setup_mailer #233

---
+++ StartupMaps/blog/app/mailers/user_mailer.rb
@@ -0,0 +1,38 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+  default from: 'notifications@example.com'
+ 
+  # Definition: This methods takes as input an entity and retieves its email to send a creation confirmation email to the entity.
+  # Input: entity
+  # Output: void
+  # Author: Omar El Menawy
+
+  def confirmation_instructions(entity)
+    @entity = entity
+    @url  = 'http://example.com/login'
+    mail(to: @entity.email, subject: 'Welcome to Startups!')
+  end
+
+
+  # Definition: This methods takes as input an entity and retieves its email to send a reset password email to the entity.
+  # Input: entity
+  # Output: void
+  # Author: Omar El Menawy
+
+  def reset_password_instructions(entity)
+    @entity = entity
+    mail :to => sign_up.email, :subject => "Password Reset"
+  end
+
+
+  # Definition: This methods takes as input an entity and retieves its email to send a unlock account email to the entity.
+  # Input: entity
+  # Output: void
+  # Author: Omar El Menawy
+
+  def unlock_instructions(entity)
+    @entity = entity
+    @url  = 'http://example.com/login'
+    mail(to: @entity.email, subject: 'Unlock Instructions')
+  end
+end
