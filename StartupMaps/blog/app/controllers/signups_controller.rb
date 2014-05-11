bcafc28f8e78b2bc1d753cec131073d51ec3b3ee
hebaabdelfattah <heba.azak@gmail.com>
2014-05-11 22:25:27 +0200

    design of the maps view, map focused on egypt, trial button 4.8_Heba_Abdelfattah_Locate_Startup_at_Signup 4.7_Heba_Abdelfattah_Map_Design #238 #241

---
+++ StartupMaps/blog/app/controllers/signups_controller.rb
@@ -0,0 +1,45 @@
+class SignupsController < ApplicationController
+
+  def new
+     #@signup.update_attribute(:lat , params[:lat]) 
+  end
+
+def show
+end
+#  def show
+ #   @signup = Signup.find(params[:id])
+  #end
+   def create
+    s = Signups.new
+    s.lat = params[:lat]
+    s.lng = params[:lng]
+    s.save
+
+      #@latit = params[:lat]
+     #@lngit = params[:lng]
+#   @signup = Signup.new(signup_params)
+ #  respond_to do |format|
+  #    if @signup.save
+   #     format.html { redirect_to @signup, notice: 'Longitute and Latitude successfully added.' }
+    #    format.json { render action: 'show', status: :created, location: @signup }
+     # else
+      #  format.html { render action: 'new' }
+       # format.json { render json: @signup.errors, status: :unprocessable_entity }
+     # end
+    #end
+# @gmapsoptions = {
+ #   "map_options" => {"center_latitude" => 27, 
+  #                     "center_longitude" => 30,
+   #                    "detect_location" => false,
+    #                   "center_on_user" => false,
+     #                  "auto_adjust" => false,
+      #                 "auto_zoom" => true,
+       #                "zoom" => 8 }
+        #         }
+        #@signup.update_attribute(:lat,);         
+  #end
+
+  #def signup_params
+   #  params.require(:signup).permit(:lat, :lng)
+   end
+end
