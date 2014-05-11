3c19588ebda8a0a73a4a7f51261032f6d98c90d4
Ziad-abdelkader <ziad.abdelkader9@gmail.com>
2014-05-11 23:59:07 +0200

    added the documentation, tests left

--- StartupMaps/blog/app/models/send_merge_request.rb
+++ StartupMaps/blog/app/models/send_merge_request.rb
@@ -3,10 +3,26 @@ class SendMergeRequest < ActiveRecord::Base
 	has_and_belongs_to_many :receivers, :class_name => "entity"
 	has_and_belongs_to_many :project
 
+	
+   # Deifination: Get the sender id from the table where the reciever id is the session id.
+   # Input: Sender id, reciever id.
+   # Output: Void.
+   # Authored: Ziad.
+
 	def self.get_sender(entity_id)
       sender_ids = SendMergeRequest.select(:sender_id).where(:receivers_id => entity_id)
       return Entity.find(sender_ids)
 	end	
+
+ 
+   # Deifination: Changes the response either to accept or reject with changing the response to 1 or 0.
+   # Comment: If accepted, it add the entity id to the project table.
+   # Input: Sender id, reciever id.
+   # Output: Void.
+   # Authored: Ziad.
+
+
+
 
 	def self.accept(reciever, sender, response)
 		if(response == 1)
