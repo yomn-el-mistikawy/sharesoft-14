f18bd4e2b6395939f6e66fca92b389eb7b5b43a1
sarah-fathallah <sfathallah.sf@gmail.com>
2014-05-12 18:07:42 +0200

    Models modified accordig to devise #237

--- StartupMaps/blog/app/models/message.rb
+++ StartupMaps/blog/app/models/message.rb
@@ -1,9 +1,11 @@
 class Message < ActiveRecord::Base
-  attr_accessible :subject, :body, :sender, :recepient, :read
+ attr_accessible :subject, :body, :sender, :recepient, :read
 
   validates_presence_of :subject, :body, :recepient
   validates :subject, length: { minimum: 3, maximum: 35 }
   validates :body, length: { minimum: 3, maximum: 1000 }
 
   belongs_to :entity
+
+
 end
