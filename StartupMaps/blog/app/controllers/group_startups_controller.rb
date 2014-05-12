50a1c8f2510424f3d705141b837ca6f18e443d9f
topser <essamazzam@ubuntu.ubuntu-domain>
2014-05-12 17:12:06 +0200

    Now when the user presses Join Group it updates the database with id of the current signed in user and the current id of the group that the user pressed joined on

--- StartupMaps/blog/app/controllers/group_startups_controller.rb
+++ StartupMaps/blog/app/controllers/group_startups_controller.rb
@@ -3,23 +3,49 @@ class GroupStartupsController < ApplicationController
 		    @groups = Group.all
 
     end
+    def join_group
+      #@group_startup.create
+  @startup_id=current_entity.id
+  #GroupStartup.create(group_id: params[:group_id], user_id: @user_id)
+  @group_startup=GroupStartup.new(group_id: params[:group_id], startup_id: @startup_id)
+
+
+    end
+    def leave_group
+       @group_startup.destroy
+    respond_to do |format|
+      format.html { redirect_to group_startups_url }
+      format.json { head :no_content }
+    end
+
+    end
 
     def create
     @startup_id=1
   #GroupStartup.create(group_id: params[:group_id], user_id: @user_id)
+
   @group_startup=GroupStartup.new(group_id: params[:group_id], startup_id: @startup_id)
   if @group_startup.save
-       #format.html { redirect_to @group_startup, notice: 'You have successfully joined group' }
-       #redirect_to("groups")
+      respond_to do |format|
+      if @group_startup.save
+        format.html { redirect_to @group_startup, notice: 'You Have Successfully Joined Group' }
        #format.json { render action: 'show', status: :created, location: @group }
+    end
+    end
+
    end
     end
 
     def check_group_startups
     if GroupStartup.where(:group_id => params[:group_id],:startup_id => @startup_id).present?
-return true
+return false
 else
-	return false
+return true
 end
-    end
+end
+
+def show
+end
+
+
 end
