810860e0a1716080d45af44b9f2fa9e0a0fcbb00
sarah-fathallah <sfathallah.sf@gmail.com>
2014-05-12 22:34:16 +0200

    Routes added Friendship #118 and #119

--- StartupMaps/blog/config/routes.rb
+++ StartupMaps/blog/config/routes.rb
@@ -19,6 +19,8 @@ Blog::Application.routes.draw do
     get "merge_request"
   end  
 
+  resources :friendships
+
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
 
