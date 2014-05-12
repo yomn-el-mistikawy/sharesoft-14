0db25b92cbe171c72191fff466ccc1ad95c203e3
AdelZee <adel.zee93@gmail.com>
2014-05-12 13:21:36 +0200

    The tables now get updated with the preferences. #226

--- StartupMaps/blog/app/models/tag.rb
+++ StartupMaps/blog/app/models/tag.rb
@@ -3,28 +3,20 @@ class Tag < ActiveRecord::Base
 
 	def self.update_pref (other, prefs)
 		prefs.to_a.each do |prefer|
-			if ((Tag.find_by tags: prefer) == nil)
+			if (((Tag.find_by tags: prefer) == nil) || (((Tag.find_by tags: prefer).tags == prefer) && (Tag.find_by entity_id: 1).entity_id != 1))
 				Tag.create(:entity_id => 1, :tags => prefer)
 			end
-
-
-
-
-
-
-
-			# if ((Tag.find_by tags: prefer) != 'nil' && (Tag.find_by)
-			# 	p 'false'
-			# end
-			# if (((Tag.find_by tags: prefer) != prefer) && (Tag.find_by tags: prefer) == 1)
-			# 	p "Hey!"
-				# Tag.create(:entity_id => 1, :tags => prefer)
 		end
-		# if other != ""
-		# 	if (((Tag.find_by tags: other).tags != other) && (Tag.find_by tags: other).entity_id != 1)
-		# 		Tag.create(:entity_id => 1, :tags => other)
-		# 	end
-		# end
+		if other != ""
+			if (((Tag.find_by tags: other) == nil) || (((Tag.find_by tags: other).tags != other) && (Tag.find_by tags: other).entity_id != 1))
+				Tag.create(:entity_id => 1, :tags => other)
+			end
+		end
+	end
+
+	def self.get_options (entity_id)
+		@options = Tag.find(:all)
+		p @options
 	end
 	
 end
