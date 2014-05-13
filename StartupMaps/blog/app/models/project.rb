25bddea95b9cc9fa6338293cddda8951c50ff14d
Hana Magdy <hana.hosny94@gmail.com>
2014-05-13 14:59:02 +0200

    model amendments in launch #133

--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -23,8 +23,8 @@ class Project < ActiveRecord::Base
   # Changes the status of a project and redirects to the project's 
   # page (show project) on success or error
   # update the status of launch project from unlaunch to launched and vice versa.
-  # Input: Void.
-  # Output: Void "it's an action" returns the success 
+  # Input: project_id/Void.
+  # Output: project_id/Void "it's an action" returns the success 
   # of the changeable button of launch upon of it's previous status. 
   # Author: Hana Magdy.
 
