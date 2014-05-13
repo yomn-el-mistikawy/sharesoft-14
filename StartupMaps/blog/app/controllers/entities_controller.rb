b0ae2498573876d3d384f26d668865dfdd4b880a
OlaEnaba <ola.enaba@gmail.com>
2014-05-13 17:34:27 +0200

    editing documentation and implementation #244

--- StartupMaps/blog/app/controllers/entities_controller.rb
+++ StartupMaps/blog/app/controllers/entities_controller.rb
@@ -98,34 +98,34 @@ class EntitiesController < ApplicationController
 end
 
 
- # Definition: Knows the user ID from the params and finds its last work status.
+  # Definition: Knows the user ID from the params and finds its last work status.
   # Input: Group ID.
   # Output: Changes to online or offline.
   # Author: Ola Enaba.
 
-def change_company_status
-      @startup = Startup.find(params[:startup_id])
-      @new = Startup.new
-      @new.id =  @startup.id
-      @new.longitude =  @startup.longitude
-      @new.latitude = @startup.latitude
-      @new.initiatior_name = @startup.initiatior_name
-      @new.milestones = @startup.milestones
-      @new.entity_id = @startup.entity_id
-      @new.location = @startup.location
-      @new.name = @startup.name
-      @new.online_status = @startup.online_status
-      @new.launch_status = @startup.launch_status
-      @new.joint_ventures = @startup.joint_ventures
-      @new.sector = @startup.sector
-      @new.number_of_working_years = @startup.number_of_working_years
-      @new.created_at = @startup.created_at
-      @new.updated_at = @startup.updated_at
-      @new.company_status = ! @startup.company_status
-      @startup.delete
-      if @new.save
-        notice = 'successful edit'
-      else
-        render 'edit'
-      end
-end
+  def change_company_status
+    @startup = Startup.find(params[:startup_id])
+    @new = Startup.new
+    @new.id =  @startup.id
+    @new.longitude =  @startup.longitude
+    @new.latitude = @startup.latitude
+    @new.initiatior_name = @startup.initiatior_name
+    @new.milestones = @startup.milestones
+    @new.entity_id = @startup.entity_id
+    @new.location = @startup.location
+    @new.name = @startup.name
+    @new.online_status = @startup.online_status
+    @new.launch_status = @startup.launch_status
+    @new.joint_ventures = @startup.joint_ventures
+    @new.sector = @startup.sector
+    @new.number_of_working_years = @startup.number_of_working_years
+    @new.created_at = @startup.created_at
+    @new.updated_at = @startup.updated_at
+    @new.company_status = ! @startup.company_status
+    @startup.delete
+    if @new.save
+      notice = 'successful edit'
+    else
+      render 'edit'
+    end
+  end
