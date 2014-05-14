7f371da972bdb40429715cbde3ff7c012f242485
Hana Magdy <hana.hosny94@gmail.com>
2014-05-14 11:54:58 +0200

    Added_few_tests #216

--- StartupMaps/blog/app/models/project.rb
+++ StartupMaps/blog/app/models/project.rb
@@ -14,35 +14,4 @@ class Project < ActiveRecord::Base
   # Cascade deletion to all associations
   accepts_nested_attributes_for :project_targets, :allow_destroy => true
   accepts_nested_attributes_for :project_requirements, :allow_destroy => true
-
-
-  # Definition: "A startup can see a list of his projects" 
-  # This method allows you to get a list of projects and 
-  # compares the project's id with the startup's id 
-  # then start listing all the projects to a specific startup.
-  # Input: entity_id, startup_id, project_id.
-  # Output: project_id "all project description in a list".
-  # Author: Hana Magdy.
-
-  def  self.listing_projects(startup)
-    @startups_listing_projects = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
-    Project.where(:id => @startups_listing_projects)
-
-  end
-
-
-  # Defintion: This method takes a project as input
-  # and searches the projects table for projects of
-  # same geographical location and category and sends
-  # the results to the suggest function in the project
-  # controller.
-  # Input: project
-  # Output: a list of suggested projects
-  # Author: Yomn El-Mistikawy
-
-  def  self.get_suggest(project, startup)
-    @projects_owned_by_startup_ids = StartupHaveProject.select(:project_id).where(:startup_id => startup.id)
-    @suggested_projects = Project.where(:location => project.location, :category => project.category)
-                                 .where.not(:id => project.id, :id => @projects_owned_by_startup_ids)
-  end
 end
--- StartupMaps/blog/spec/models/project_spec.rb
+++ StartupMaps/blog/spec/models/project_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Project do
-  
+  it "Creates project" do
+    project = Project.new(
+    	name: "Hana",
+    	description: "Hana's project",
+    	category: "Clothes")
+    expect(project).to be_valid
+  end
+
+  it "inserts into ProjectsStartup" do
+       project = Project.new(
+    	name: "Hana",
+    	description: "Hana's project",
+    	category: "Clothes")
+       startup = Startup.create(name: "Hana")
+       project_startup = ProjectsStartup.create(startup_id: startup.id, project_id: project.id)
+       expect(ProjectsStartup.where(startup_id: startup.id)).to eq([project_startup])
+  end	
 end
