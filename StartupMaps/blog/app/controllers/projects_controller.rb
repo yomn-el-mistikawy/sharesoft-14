71ef1e4201752476e32d5da1e50d6b5df3e2923a
Hana Magdy <hana.hosny94@gmail.com>
2014-05-15 01:45:45 +0200

    please find launch working with no test as it doesn't need some #133

--- StartupMaps/blog/app/controllers/projects_controller.rb
+++ StartupMaps/blog/app/controllers/projects_controller.rb
@@ -1,48 +1,23 @@
 class ProjectsController < ApplicationController
 
-  # == Begin  == 
-  # Definition: "A startup can see a list of his projects" 
-  # This method allows you to get a list of projects and 
-  # moves with a session_id of an entity in order to 
-  # list all the project's of the startup
-  # that belong to a specific startup and is linked to the index HTML file
+  # Definition:This method shows all details of a project
+  # that belong to a specific startup and is linked to the show HTML file
+  # which also includes the editing part
   # respond_to --> gives a direct access to the HTML/XML/PDF whatever is it
   # it's reachable and knows what's happening in the file.
-  # Input: entity_id, startup_id.
-  # Output: project_id "all project description in a list".
+  # Input: Project_id.
+  # Output: Project_id "all project description (before and after editing)".
   # Author: Hana Magdy.
 
-  def index
-    session[:entity_id] = 1
-    @projects = Project.listing_projects(Startup.find(session[:entity_id]))
-    # @projects = Project.all
-    # @startup = Startup.find(session[:entity_id])
-    # @projects = @startup.project.find( :all)
+  def show
+    @project = Project.find(params[:id])
 
     respond_to do |format|
       format.html
     end
   end
 
-
-  # Defintion: This method takes the project_id and session id
-  # as input and calls get_suggested() to show a list of 
-  # projects with the same geographical location and
-  # category to the startup's opened project.The suggestions 
-  # are only shown for project owners. Moreover, if there are no 
-  # suggestions, a message is shown stating so.
-  # Input: project_id, entity_id
-  # Output: project_id, suggested_project
-  # Author: Yomn El-Mistikawy
-
-  def suggest
-    if (StartupHaveProject.check_ownership(Project.find(params[:project_id]), Startup.find(session[:entity_id])).size != 0)
-      @suggested = Project.get_suggest(Project.find(params[:project_id]), Startup.find(session[:entity_id]))
-    else
-      render text: ""
-    end
-  end
-
+  
   # Definition: "A startup can launch its project"
   # Changes the status of a project and redirects to the project's 
   # page (show project) on success or error
@@ -66,24 +41,6 @@ class ProjectsController < ApplicationController
       end
 
       format.html { redirect_to project }
-    end
-  end
-
-  
-  # Definition:This method shows all details of a project
-  # that belong to a specific startup and is linked to the show HTML file
-  # which also includes the editing part
-  # respond_to --> gives a direct access to the HTML/XML/PDF whatever is it
-  # it's reachable and knows what's happening in the file.
-  # Input: Project_id.
-  # Output: Project_id "all project description (before and after editing)".
-  # Author: Hana Magdy.
-
-  def show
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      format.html
     end
   end
 end
