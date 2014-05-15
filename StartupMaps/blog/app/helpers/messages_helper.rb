75757ffd3bd269b038c7cfb825440256a03b9016
sarah-fathallah <sfathallah.sf@gmail.com>
2014-05-15 12:37:44 +0200

    Helper indentation and documentation fixed #237

--- StartupMaps/blog/app/helpers/messages_helper.rb
+++ StartupMaps/blog/app/helpers/messages_helper.rb
@@ -1,4 +1,10 @@
 module MessagesHelper
+
+	# Definition: Checks if the message if read or not.
+  # Input: Message.
+  # Output: If read marks at read.
+  # Author: Sarah Fathallah. 
+
   def marked_as_read(message)
     if @message.read != 1 
       @message.read = 1
