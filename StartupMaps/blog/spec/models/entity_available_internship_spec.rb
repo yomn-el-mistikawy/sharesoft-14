32bd19578a5f6e7b12d1911823861e2c2d418132
AdelZee <adel.zee93@gmail.com>
2014-05-15 18:01:45 +0200

    Fixed the test files. #143

--- StartupMaps/blog/spec/models/entity_available_internship_spec.rb
+++ StartupMaps/blog/spec/models/entity_available_internship_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe EntityAvailableInternship do
 	it "change internship status" do
-    entity = Entity.new(name: "adel")
-    entity_intern = EntityAvailableInternship.new(name: entity.name, available: 0, entity_id: entity.id)
-    expect(Entity.all.size).to eq(1)
+    entity = Entity.create(name: "adel", email: "adel@gmail.com", password: "12345678")
+    entity_intern = EntityAvailableInternship.create(name: "entity.name", available: 0, entity_id: "entity.id")
     expect(EntityAvailableInternship.all.size).to eq(1)
+    expect(Entity.all.size).to eq(3)
 	end
 end
