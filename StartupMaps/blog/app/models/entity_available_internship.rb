6553452e8939e50eb52cbaac7a69a8f6f40fd20c
AdelZee <adel.zee93@gmail.com>
2014-05-15 19:28:30 +0200

    Added documentation to the model. #143

--- StartupMaps/blog/app/models/entity_available_internship.rb
+++ StartupMaps/blog/app/models/entity_available_internship.rb
@@ -1,6 +1,13 @@
 class EntityAvailableInternship < ActiveRecord::Base
   belongs_to :entity
 
+
+  # Definition: This method updates the attribute in the table entity_available_internships
+  # If it's true, it makes it false, and if it's false, it makes it true.
+  # Input: Entity_status, entity.
+  # Output: Void.
+  # Author: Adel Badawy.
+
   def self.change_internship(entity_status, entity)
       if entity_status.update_attribute(:available, !entity_status.available)
         puts "Successfully changed!"
