5bec73daabcc4a2e0dce1232fcfda551dd19cbc1
Nano <nano@ubuntu.(none)>
2014-05-16 13:07:37 -0400

    rspec tests

--- StartupMaps/blog/spec/models/post_spec.rb
+++ StartupMaps/blog/spec/models/post_spec.rb
@@ -6,13 +6,13 @@ describe Post do
 		id: "1",
 		title: "this is a title",
 		text: "this is a post",
-		group_id: "1",
-		startup_id: "1"
+		group_id: '1',
+		startup_id: '1'
 	}
 end
 
 it "is not valid with an id" do
-	post = Post.new(@valid_post.merge!(:id => "1"))
+	post = Post.new(@valid_post.merge!(:id => '1'))
 	expect(post).to_not be_valid
 end
 
@@ -27,12 +27,12 @@ it "is not valid with a text" do
 end
 
 it "is not valid with a group id" do
-	post= Post.new(@valid_post.merge!(:group_id => "1"))
+	post= Post.new(@valid_post.merge!(:group_id => '1'))
 	expect(post).to_not be_valid
 end
 
 it "is not valid with a startup id" do
-	post= Post.new(@valid_post.merge!(:startup_id => "1"))
+	post= Post.new(@valid_post.merge!(:startup_id => '1'))
 	expect(post).to_not be_valid
 end
 
@@ -40,19 +40,19 @@ it "is invalid without an id" do
 	expect(Post.new(id: nil)).to have(1).errors_on(:id)
 end
 
-it "is invalid without an title" do
+it "is invalid without a title" do
 	expect(Post.new(title: nil)).to have(1).errors_on(:title)
 end
 
-it "is invalid without an text" do
+it "is invalid without a text" do
 	expect(Post.new(text: nil)).to have(1).errors_on(:text)
 end
 
-it "is invalid without an group id" do
+it "is invalid without a group id" do
 	expect(Post.new(group_id: nil)).to have(1).errors_on(:group_id)
 end
 
-it "is invalid without an startup id" do
+it "is invalid without a startup id" do
 	expect(Post.new(startup_id: nil)).to have(1).errors_on(:startup_id)
 end
 
