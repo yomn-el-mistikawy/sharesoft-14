f33a0781361ea817edfd8d3ebc012333ee996f71
BIG JOE <youssef@ubuntu.(none)>
2014-05-16 06:15:30 -0400

    User can enable or disable email notifications with a buttom #246

--- StartupMaps/blog/app/models/comment.rb
+++ StartupMaps/blog/app/models/comment.rb
@@ -2,4 +2,29 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :startup
   has_many :likes, :as => :likable
+
+  # Definition: This methods calls create method in the group controller to send with a notification email with the comment.
+  # Input: Comments.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_save:create_comment_email
+  def create_comment_email
+    if @group.notification?
+      UserMailer.comment_email(@comment).deliver
+    end
+  end
+  
+
+  # Definition: This methods calls update method in the group controller to send with a notification email with the comment.
+  # Input: Comments.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_update:update_comment_email
+  def update_comment_email
+    if @group.notification?
+      UserMailer.comment_email(@comment).deliver
+    end
+  end
 end
--- StartupMaps/blog/app/models/post.rb
+++ StartupMaps/blog/app/models/post.rb
@@ -3,4 +3,29 @@ class Post < ActiveRecord::Base
   belongs_to :startup
   has_many :comments
   has_many :likes, :as => :likable
+
+  # Definition: This methods calls create method in the group controller to send with a notification email with the post.
+  # Input: Comments.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_save:create_post_email
+  def create_post_email
+    if @group.notification?
+      UserMailer.post_email(@post).deliver
+    end
+  end
+  
+
+  # Definition: This methods calls update method in the group controller to send with a notification email with the post.
+  # Input: Comments.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_update:update_post_email
+  def update_post_email
+    if @group.notification?
+      UserMailer.post_email(@post).deliver
+    end
+  end
 end
