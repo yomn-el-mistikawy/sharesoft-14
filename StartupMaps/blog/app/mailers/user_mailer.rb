a4635e5442769243be698a7c73b9bf6984177057
BIG JOE <youssef@ubuntu.(none)>
2014-05-16 05:47:10 -0400

    When member of group posts or comments an email is sent #245

--- StartupMaps/blog/app/mailers/user_mailer.rb
+++ StartupMaps/blog/app/mailers/user_mailer.rb
@@ -1,17 +1,62 @@
 class UserMailer < ActionMailer::Base
-  default :from => "youssefattaby1993@gmail.com"
+  default from: "from@example.com"
+  default from: 'notifications@example.com'
+ 
+  # Definition: This methods takes as input an entity and retieves its email to send a creation confirmation email to the entity.
+  # Input: Entity.
+  # Output: Void.
+  # Author: Omar El Menawy.
+
+  def confirmation_instructions(entity)
+    @entity = entity
+    @url  = 'http://example.com/login'
+    mail(to: @entity.email, subject: 'Welcome to Startups!')
+  end
+
+
+  # Definition: This methods takes as input an entity and retieves its email to send a reset password email to the entity.
+  # Input: Entity.
+  # Output: Void.
+  # Author: Omar El Menawy.
+
+  def reset_password_instructions(entity)
+    @entity = entity
+    mail :to => sign_up.email, :subject => "Password Reset"
+  end
+
+
+  # Definition: This methods takes as input an entity and retieves its email to send a unlock account email to the entity.
+  # Input: Entity.
+  # Output: Void.
+  # Author: Omar El Menawy.
+
+  def unlock_instructions(entity)
+    @entity = entity
+    @url  = 'http://example.com/login'
+    mail(to: @entity.email, subject: 'Unlock Instructions')
+  end
+
+
+  # Definition: This methods takes as input a post and an entity which retieves its email to send a notification of the post that was created orupdated in the group.
+  # Input: Entity, Post.
+  # Output: Void.
+  # Author: Youssef Attaby.
 
   def post_email(post, entity)
-  	@post=post
-  	mail(:to => entity.e_mail, :subject => "Post email")
- end
- def comment_email(comment, entity)
- 	@comment = comment
- 	mail(:to => entity.e_mail, :subject => "Comment email")
- end
- def group_email(group, entity)
- 	@group = group
- 	mail(:to => entity.e_mail, :subject => "Group email")
- end
+    @post=post
+    @email= Entity.find_by(email: entity.email).email
+    mail(to: @email, subject: 'Post Email')
+  end
 
 
+  # Definition: This methods takes as input a comment and an entity which retieves its email to send a notification of the post that was created orupdated in the group.
+  # Input: Entity, Comment.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  def comment_email(post, entity)
+    @comment=comment
+    @email= Entity.find_by(email: entity.email).email
+    mail(to: @email, subject: 'Comment Email')
+  end
+end
--- StartupMaps/blog/app/models/post.rb
+++ StartupMaps/blog/app/models/post.rb
@@ -3,4 +3,28 @@ class Post < ActiveRecord::Base
   belongs_to :startup
   has_many :comments
   has_many :likes, :as => :likable
+
+  # Definition: This methods calls create method in the group controller to send with a notification email with the post.
+  # Input: Posts.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_save:create_post_email
+  def create_post_email
+  	if @group.notification?
+        UserMailer.post_email(@post).deliver
+    end
+  end
+
+  # Definition: This methods calls create method in the group controller to send with a notification email with the post.
+  # Input: Posts.
+  # Output: Void.
+  # Author: Youssef Attaby.
+
+  after_update:update_post_email
+  def update_post_email
+    if @group.notification?
+      UserMailer.post_email(@post).deliver
+    end
+  end
 end
