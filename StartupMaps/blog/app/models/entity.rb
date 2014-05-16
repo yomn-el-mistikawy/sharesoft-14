d90a9a940b1aa657c59cecc1fe98becee634b772
Sarah Fathallah <sfathallah.sf@gmail.com>
2014-05-16 10:47:38 +0300

    Update entity.rb

--- StartupMaps/blog/app/models/entity.rb
+++ StartupMaps/blog/app/models/entity.rb
@@ -23,11 +23,5 @@ class Entity < ActiveRecord::Base
   is_impressionable
   TYPES = %w[Startup Investor Service]
   SECTORS = %w[Agriculture Manufacturing Trading Clothes Telecommunications]
-
-
-has_many :friendships
-has_many :friends, :through => :friendships
-has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
 
